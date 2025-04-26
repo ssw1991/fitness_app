c1649fbb711e1fcf99360e44e7d38e0cbe38e182
ssw1991 <wilson.s.shilo@gmail.com>
2025-04-26 14:48:37 -0400

    moved construction to UserResponse

--- userservice/userservice/src/main/java/com/fitness/userservice/dto/UserResponse.java
+++ userservice/userservice/src/main/java/com/fitness/userservice/dto/UserResponse.java
@@ -1,4 +1,5 @@
 package com.fitness.userservice.dto;
+import com.fitness.userservice.model.User;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -6,6 +7,20 @@ import java.time.LocalDateTime;
 @Data
 public class UserResponse {
 
+    public UserResponse(User user){
+        /*
+        The inclusion of this constructor, removes all automatically generated constructors
+        delivered by the @Data annotation.  This may at somepoint require the inclusion of
+        a required args constructor if it becomes  neccessary.
+         */
+        id = user.getId();
+        email = user.getEmail();
+        password = user.getPassword();
+        firstName = user.getFirstName();
+        lastName = user.getLastName();
+        createdAt = user.getCreatedAt();
+        updatedAt = user.getCreatedAt();
+    }
     private String id;
     private String email;
     private String password;
